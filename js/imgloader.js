6cd2cb1d9bce4c67142cc76821462794e0ac153e
CameronC17 <cameronchalmers@hotmail.co.uk>
2016-08-31 19:02:10 +0100

    got the image loading locally, legit

--- js/imgloader.js
+++ js/imgloader.js
@@ -1,25 +1,31 @@
 window.onload = function() {
 
-document.getElementById('clicky').onclick = function() {
+/*document.getElementById('clicky').onclick = function() {
    alert("button was clicked");
- };
+ };*/
 
- document.getElementById('imgUpload').onchange = function(e) {
+document.getElementById('imgUpload').onchange = function(e) {
    var file = e.srcElement.files[0];
 
-   var img = document.createElement("img");
    var reader = new FileReader();
    reader.onloadend = function() {
-        img.src = reader.result;
+        document.getElementById('mainImage').src = reader.result;
+        document.getElementById('mainImage').style.height = "600px";
+        //fade out animation
+        var op = 1;
+        var timer = setInterval(function () {
+            if (op <= 0){
+                clearInterval(timer);
+                document.getElementById('loadImagePanel').style.display = 'none';
+            }
+            document.getElementById('loadImagePanel').style.opacity = op;
+            document.getElementById('loadImagePanel').style.filter = 'alpha(opacity=' + op * 100 + ")";
+            op -= op * 0.1;
+        }, 30);
    }
    reader.readAsDataURL(file);
 
-   document.getElementById('loadImagePanel').after(img);
-   //document.getElementById('mainImage').src = img.src;
-   //document.getElementById('loadImagePanel').style.display = "none";
-
-  };
-
+ };
 
 
 }
